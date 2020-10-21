b2e4ee0e98269b07bb968a2b6aef0f62ba97c528
Jonathan Prieto <jonathan.prieto@me.com>
2020-10-21 10:13:37 -0500

    Usuario

--- src/main/java/lab10/Usuario.java
+++ src/main/java/lab10/Usuario.java
@@ -1,8 +1,11 @@
 package lab10;
 
+import java.util.Date;
+
 public class Usuario {
-    String hash,sexo,primerNombre;
+    String hash, primerNombre, apellidoPaterno, apellidoMaterno, email, nombre, passwd, segundoNombre, dni, nombreCompleto;
     Long codigo;
-    Integer vigencia;
+    Integer sexo, vigencia;
+    Date fechaCese;
 
 }
