beca83ce0192f7c89618b83a136ab0191ad88886
Jonathan Prieto <jonathan.prieto@me.com>
2020-10-22 10:03:13 -0500

    Create Main

---
+++ src/main/java/lab10/ServiceApplication.java
@@ -0,0 +1,12 @@
+package lab10;
+import java.util.logging.*;
+
+public class ServiceApplication {
+
+    static final Logger logger = Logger.getLogger(Usuario.class.getName());
+
+    public static void main(String[] args) {
+        logger.info("Empezo!");
+    }
+
+}
