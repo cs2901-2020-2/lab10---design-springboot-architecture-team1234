3b1751706676bb30685c7c489c8d93f5b2b661a7
Jonathan Prieto <jonathan.prieto@me.com>
2020-10-22 08:51:43 -0500

    AuthenticationController Arquitectura

--- src/main/java/lab10/AuthenticationController.java
+++ src/main/java/lab10/AuthenticationController.java
@@ -1,4 +1,23 @@
 package lab10;
 
+import org.springframework.http.ResponseEntity;
+
 public class AuthenticationController {
+    private UsuarioService service;
+
+    public AuthenticationController() {}
+
+    public ResponseEntity<?> create(Usuario user){
+    }
+
+    public ResponseEntity read(Long codigo){
+    }
+
+    public ResponseEntity<?> update(Long codigo, Usuario user){
+
+    }
+
+    public ResponseEntity<?> delete(Long codigo){
+
+    }
 }
