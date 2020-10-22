8568a19ce969891370939ef18899821b8fabaa5b
mauriciopinto <mauricio.pinto@utec.edu.pe>
2020-10-22 09:50:08 -0500

    authcont

--- src/main/java/lab10/AuthenticationController.java
+++ src/main/java/lab10/AuthenticationController.java
@@ -1,23 +1,40 @@
 package lab10;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+@RestController
+@RequestMapping ("/users")
 public class AuthenticationController {
     private UsuarioService service;
-
+    RestTemplate restTemplate = new RestTemplate();
     public AuthenticationController() {}
 
-    public ResponseEntity<?> create(Usuario user){
+    @PostMapping
+    public ResponseEntity<?> create(Usuario user) {
+        ResponseEntity<String> entity = restTemplate.getForEntity ("/users", String.class);
+        service.create(user);
+        return entity;
     }
 
-    public ResponseEntity read(Long codigo){
+    public ResponseEntity<?> read(Long codigo){
+        ResponseEntity<String> entity = restTemplate.getForEntity("/users", String.class);
+        service.findOne(codigo);
+        return entity;
     }
 
     public ResponseEntity<?> update(Long codigo, Usuario user){
-
+        ResponseEntity<String> entity = restTemplate.getForEntity("/users", String.class);
+        service.update(user);
+        return entity;
     }
 
     public ResponseEntity<?> delete(Long codigo){
-
+        ResponseEntity<String> entity = restTemplate.getForEntity("/users", String.class);
+        service.delete(codigo);
+        return entity;
     }
 }
